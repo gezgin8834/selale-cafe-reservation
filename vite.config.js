1de3fbf75e7e35bf793599fc4705a29395042975
gezgin8834 <you@example.com>
2025-07-14 00:09:43 +0300

    Gerekli dosya güncellemeleri

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/selale-cafe-reservation/", // ✅ doğru base yolu
+  base: "/cafe-reservation-system/", // ✅ doğru base yolu
 });
