2bf59fc1fc1fdb4951a959b66bc6cfab34cf6b6f
gezgin8834 <gezgin8834@outlook.com>
2025-07-13 15:05:46 +0300

    İlk commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    historyApiFallback: true,
+  base: "/Selale Cafe/", // bu satır da önemli!
+  build: {
+    outDir: "build",
   },
+  server: {
+    historyApiFallback: true
+  }
 })
