d1c95b0ca9bb07791333977d7076d0682ae23e9e
gezgin8834 <you@example.com>
2025-07-14 15:51:40 +0300

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    plugins: [react()],
     base: "/cafe-reservation-system/", // ✅ doğru base yolu
-
+plugins: [react()],
 });
