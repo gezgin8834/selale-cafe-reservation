596fba4603a8c6784b70b4207ac234aaffe6a676
gezgin8834 <you@example.com>
2025-07-15 13:31:10 +0300

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: "/cafe-reservation-system/", // ✅ doğru base yolu
-plugins: [react()],
+  plugins: [react()],
+  base: process.env.NODE_ENV === 'production' && process.env.BUILD_ENV === 'github'
+    ? '/cafe-reservation-system/'
+    : '/',
 });
