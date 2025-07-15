5b91db32ee00dc61b1a549bf3e2a4afc7bd3764c
gezgin8834 <you@example.com>
2025-07-15 13:51:57 +0300

    Fix: vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-// base: process.env.BUILD_ENV === 'github' ? '/cafe-reservation-system/' : '/'
-
+    base: "/cafe-reservation-system/", // ✅ doğru base yolu
+plugins: [react()],
 });
