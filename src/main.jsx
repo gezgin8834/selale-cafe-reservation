be8ad846f61dfb8cd7eee6443193edd4a6032ad0
gezgin8834 <you@example.com>
2025-07-19 16:30:40 +0300

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,15 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
-import { BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
-
-
- createRoot(document.getElementById('root')).render(
- <React.StrictMode>
-    <BrowserRouter basename="/cafe-reservation-system">
+const rootElement = document.getElementById('root');
+createRoot(rootElement).render(
+  <React.StrictMode>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
 );
-
-export default App;
